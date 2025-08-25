ada630f5dce3da45924a1025843e39806bb1fd31
FHsm97 <hashemi.fatemeh1379@gmail.com>
2025-08-25 13:33:08 +0330

    S2-E7-link component

--- src/app/series/page.tsx
+++ src/app/series/page.tsx
@@ -1,10 +1,20 @@
+import Link from "next/link";
 
 
-export default function seriesPage() {
+export default function SeriesPage() {
 
     return (
         <>
+            <Link href="/">Back</Link>
             <h1>Series Page</h1>
+            <ul>
+                <li>
+                    <Link href="/series/reactjs-course">ReactJs Course</Link>
+                </li>
+                <li>
+                    <Link href="/series/laravel-course">Laravel Course</Link>
+                </li>
+            </ul>
 
         </>
     )
