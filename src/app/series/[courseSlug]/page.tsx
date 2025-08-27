39565e9f5052dfbd37f400c8b76885acd4fb9292
FHsm97 <hashemi.fatemeh1379@gmail.com>
2025-08-27 10:55:29 +0330

    S2-E9-use Router-client component

--- src/app/series/[courseSlug]/page.tsx
+++ src/app/series/[courseSlug]/page.tsx
@@ -1,12 +1,29 @@
+'use client'
+
+import { useRouter } from "next/navigation";
+import { use } from "react";
 
 
-export default async function SingleSeriePage({params}:{params:{courseSlug:string}}) {
+export default function SingleSeriePage({params}:{params:Promise<{courseSlug:string}>}) {
 
-    const {courseSlug}=await params;
+    const router=useRouter();
+
+    const {courseSlug}=use(params)
+
+
+    const buyCourseHandler=()=>{
+        //validate
+        //check with api
+        router.push('/')
+    }
     return(
         <>
         <h1>Single Course Page</h1>
         <p>{courseSlug}</p>
+
+        <button onClick={buyCourseHandler}>
+            Buy the Course
+        </button>
         </>
     )
 }
