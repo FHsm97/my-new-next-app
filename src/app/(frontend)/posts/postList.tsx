970eba9d965778c4e1b484e19ff202c75388f81b
FHsm97 <hashemi.fatemeh1379@gmail.com>
2025-09-06 14:14:19 +0330

    S4-E1-CSS module

--- src/app/(frontend)/posts/postList.tsx
+++ src/app/(frontend)/posts/postList.tsx
@@ -1,5 +1,6 @@
-import Link from "next/link";
 
+import Link from "next/link";
+import postStyles from './postList.module.css'
 
 async function getPosts() {
 
@@ -25,7 +26,7 @@ export default async function PostList() {
             posts?.map((post: any) => (
                 <div key={post?.id}>
                     <Link href={`/posts/${post?.id}`}>
-                        <h2>title:{post?.title}</h2>
+                        <h2 className={postStyles.postTitle}>title:{post?.title}</h2>
                     </Link>
                     <br />
                     <hr />
