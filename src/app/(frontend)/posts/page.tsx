0808c04462aa7a67cd1ae30396abf6f1609c40ee
FHsm97 <hashemi.fatemeh1379@gmail.com>
2025-09-13 10:24:04 +0330

    S5-E6-lazy loading

--- src/app/(frontend)/posts/page.tsx
+++ src/app/(frontend)/posts/page.tsx
@@ -1,23 +1,32 @@
+'use client'
 
 
-
-import VideoPlayer from "../VideoPlayer";
+import { useState } from "react";
+// import VideoPlayer from "../VideoPlayer";
 import PostList from "./postList";
+import dynamic from "next/dynamic";
+import VideoPlayer from "../VideoPlayer";
 
 
 
 
 
-export default async function Page() {
+export default function Page() {
+    const videoPlayer=dynamic(()=>import('../VideoPlayer'))
+    // const postList=dynamic(()=>import('./postList')) it show post list but it has many errors
+    const [showPlayer,setShowPlayer]=useState(false);
+    // const [showPostList,setShowPostList]=useState(false)
 
 
     // console.log(posts)
 
     return (
         <>
-            <VideoPlayer />
+        <button onClick={()=>setShowPlayer(!showPlayer)}>toggle player</button>
+            {showPlayer&&<VideoPlayer/>}
             <h1 className="font-vazir font-bold">لیست پست‌ها</h1>
-            <PostList />
+            {/* <PostList /> */}
+            
 
 
 
